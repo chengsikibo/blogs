363844ea2694480e2a95199cf7255bf3685cdaa6
cxs040 <cxs040@example.com>
2017-05-01 14:21:07 -0500

    add view and template

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
@@ -8,3 +8,8 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blogs/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogs/post_detail.html', {'post': post})
